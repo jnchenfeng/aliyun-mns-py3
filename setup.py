2ee00c1de35c6fb156437f0e9a5c9ad5feaba74a
fengchen <feng.chen@quvideo.com>
2019-03-09 01:42:44 +0800

    aliyun mns python3

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aliyun-mns-py3",
+    version="1.0.0",
+    keywords=("pip", "aliyun", "mns"),
+    description="aliyun mns python3",
+    long_description="aliyun mns for python3",
+    license="MIT Licence",
+
+    url="https://github.com/jnchenfeng/aliyun-mns-py3",
+    author="ChenFeng",
+    author_email="jnchenfeng123@163.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
